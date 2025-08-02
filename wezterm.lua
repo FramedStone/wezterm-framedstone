98036f18cc70262ffe37a0bd1ed8724d323a71fd
vanoss22 <leeweixuan39@gmail.com>
2025-08-03 03:08:04 +0800

    chore: tokyonight colorscheme

--- wezterm.lua
+++ wezterm.lua
@@ -18,49 +18,24 @@ config = {
 	-- window
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
-	-- default_prog = { "wsl.exe", "bash", "-c", "source ~/.bashrc; nvim" },
-
-	-- cell
-	cell_width = 0.9,
-
-	-- font
-	font_size = 18.0,
-
-	-- cursor
-	colors = {
-		cursor_bg = "#bfbfbf",
-	},
-
-	background = {
-		{
-			source = {
-				File = "/Users/User/Pictures/wezterm-wallpaper.jpg", -- interstellar planet
-				-- File = "/Users/leeweixuan/Pictures/wezterm-wallpaper.gif", -- ocean with evening sun (light)
-				-- File = "/Users/leeweixuan/Pictures/wezterm-wallpaper-1.gif", -- ocean with evening sun (dimmer)
-			},
-			hsb = {
-				hue = 1.0,
-				saturation = 1.0,
-				brightness = 0.1,
-			},
-			width = "100%",
-			height = "100%",
-		},
-		{
-			source = {
-				Color = "#282c35",
-			},
-			width = "100%",
-			height = "100%",
-			opacity = 0.1,
-		},
-	},
 	window_padding = {
 		left = 10,
 		right = 0,
 		top = 0,
 		bottom = 0,
 	},
+	-- default_prog = { "wsl.exe", "bash", "-c", "source ~/.bashrc; nvim" },
+
+	-- font
+	font_size = 14.5,
+
+	-- cursor
+	colors = {
+		cursor_bg = "#bfbfbf",
+	},
+
+	window_background_opacity = 0.9,
+	color_scheme = "Tokyo Night",
 }
 
 -- keybinds
